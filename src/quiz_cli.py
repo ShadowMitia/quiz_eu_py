1fef11452a9ebf4c9917d6338a14c20aaa94540d
Dimitri Belopopsky <dimitri@belopopsky.com>
2015-08-20 03:49:09 +0200

    Refactorisation.
    La class Quiz est transféré dans son propre fichier.
    Correction d'un oubli de conversion dans l'affichage du score.

---
+++ src/quiz_cli.py
@@ -0,0 +1,67 @@
+from random import randrange
+
+
+class Quiz():
+
+    def __init__(self, members):
+        """Initialise variables and start quiz"""
+        self.app_name = "Quiz EU"
+        self.capital_question_template = "What is the capital of {}?"
+        self.date_entry_question_template = "In what year did {} join the EU?"
+        self.number_of_countries = 27
+        self.score = 0
+        self.countries = []
+        self.capital = []
+        self.date_entry = []
+        self.members = members
+        self.game()
+        print("Your score: " + str(self.score) + "/"
+              + str(self.number_of_countries))
+
+    def game(self, type="capital"):
+        """Runs the quiz
+           Args:
+                type: which type of quiz is to be run
+           Returns:
+                nothing
+        """
+        question = None
+        if type == "capital":
+            question = self.capital_question_template
+        elif type == "date_entry":
+            question = self.date_entry_question_template
+        looping = True
+        while looping:  # Weeeee
+            random_country = randrange(0, len(self.members), 1)
+            print(question.format(self.members[random_country][0]))
+            answer = input("-> ")
+            if answer in ["q", "quit", "exit"]:
+                looping = False
+                return
+            if type == "capital":
+                if self.verify_answer(answer,
+                                      self.members[random_country][1]):
+                    self.score += 1
+            elif type == "date_entry":
+                if self.verify_answer(answer,
+                                      self.members[random_country][2]):
+                    self.score += 1
+            if len(self.members) > 1:
+                self.members.remove(self.members[random_country])
+            else:
+                looping = False
+
+    def verify_answer(self, answer, real_answer):
+        """Checks the input with the real answer
+           Args:
+                answer: input
+                real_answer: the real answer
+           Returns:
+                True if the same, False otherwise
+        """
+        if answer.lower() == real_answer.lower():
+            print("Correct!")
+            return True
+        else:
+            print("Wrong answer: the answer was: " + real_answer)
+            return False
--- src/quiz_eu.py
+++ src/quiz_eu.py
@@ -1,4 +1,6 @@
-from random import randrange
+# QUIZ EU APP
+
+from quiz_cli import Quiz
 
 european_members = [
     ("Austria", "Vienna", 1995),
@@ -32,73 +34,13 @@ european_members = [
 ]
 
 
-class Quiz():
+class App():
+    pass
 
-    def __init__(self):
-        """Initialise variables and start quiz"""
-        self.app_name = "Quiz EU"
-        self.capital_question_template = "What is the capital of {}?"
-        self.date_entry_question_template = "In what year did {} join the EU?"
-        self.number_of_countries = 27
-        self.score = 0
-        self.countries = []
-        self.capital = []
-        self.date_entry = []
-        self.members = european_members
-        self.game()
-        print("Your score: " + self.score + "/" + self.number_of_countries)
-
-    def game(self, type="capital"):
-        """Runs the quiz
-           Args:
-                type: which type of quiz is to be run
-           Returns:
-                nothing
-        """
-        question = None
-        if type == "capital":
-            question = self.capital_question_template
-        elif type == "date_entry":
-            question = self.date_entry_question_template
-        looping = True
-        while looping:  # Weeeee
-            random_country = randrange(0, len(self.members), 1)
-            print(question.format(european_members[random_country][0]))
-            answer = input("-> ")
-            if answer in ["q", "quit", "exit"]:
-                looping = False
-                return
-            if type == "capital":
-                if self.verify_answer(answer,
-                                      self.members[random_country][1]):
-                    self.score += 1
-            elif type == "date_entry":
-                if self.verify_answer(answer,
-                                      self.members[random_country][2]):
-                    self.score += 1
-            if len(self.members) > 1:
-                self.members.remove(self.members[random_country])
-            else:
-                looping = False
-
-    def verify_answer(self, answer, real_answer):
-        """Checks the input with the real answer
-           Args:
-                answer: input
-                real_answer: the real answer
-           Returns:
-                True if the same, False otherwise
-        """
-        if answer.lower() == real_answer.lower():
-            print("Correct!")
-            return True
-        else:
-            print("Wrong answer: the answer was: " + real_answer)
-            return False
 
 if __name__ == "__main__":
     try:
-        Quiz()
+        Quiz(european_members)
     except EOFError:
         print()
     except KeyboardInterrupt:
